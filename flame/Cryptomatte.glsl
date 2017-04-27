e8675ce532da37ae52213692236cbd0bd5438c85
Lewis Saunders <lewis.saunders@gmail.com>
2017-04-27 22:46:11 +0100

    with 32-bit support in Flame 2018, we can enter exact ID values and it will work, but no colour picking still!

--- flame/Cryptomatte.glsl
+++ flame/Cryptomatte.glsl
@@ -1,7 +1,6 @@
 uniform sampler2D uCrypto00rgb, uCrypto00a, uCrypto01rgb, uCrypto01a, uCrypto02rgb, uCrypto02a;
-uniform vec3 id1, id2, id3;
-uniform float tolerance;
 uniform float adsk_result_w, adsk_result_h;
+uniform float id;
 
 void main() {
   vec2 res = vec2(adsk_result_w, adsk_result_h);
@@ -15,12 +14,12 @@ void main() {
   vec2 f = vec2(texture2D(uCrypto02rgb, xy).b, texture2D(uCrypto02a, xy).r);
 
   float o = 0.0;
-  if(abs(id1.r - a.x) < tolerance) o += a.y;
-  if(abs(id1.r - b.x) < tolerance) o += b.y;
-  if(abs(id1.r - c.x) < tolerance) o += c.y;
-  if(abs(id1.r - d.x) < tolerance) o += d.y;
-  if(abs(id1.r - e.x) < tolerance) o += e.y;
-  if(abs(id1.r - f.x) < tolerance) o += f.y;
+  if(a.x == id) o += a.y;
+  if(b.x == id) o += b.y;
+  if(c.x == id) o += c.y;
+  if(d.x == id) o += d.y;
+  if(e.x == id) o += e.y;
+  if(f.x == id) o += f.y;
 
   gl_FragColor = vec4(o);
 }
