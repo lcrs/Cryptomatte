d1a681fe0604f9746375e866f7d099fb881e85e5
Lewis Saunders <lewis.saunders@gmail.com>
2017-06-06 23:29:04 +0100

    Alternate preview with red/green seeded from ID, blue channel hiding real ID

--- flame/Cryptomatte.glsl
+++ flame/Cryptomatte.glsl
@@ -6,6 +6,8 @@
            limit is ever removed
 */
 
+#extension GL_EXT_gpu_shader4 : enable
+
 // Image resolution
 uniform float adsk_result_w, adsk_result_h;
 
@@ -27,6 +29,27 @@ uniform bool reminder;
 
 // Widget which is temporarily draggable over the image to inspect available mattes
 uniform vec2 inspect;
+  
+float frexp(float x, out float e) {
+  e = ceil(log2(x));
+  return(x * exp2(-e));
+}
+
+bool isnan(float f) {
+  // Try a few things.  Some drivers optimise some of them away :/
+  if(f != f) {
+    return true;
+  }
+  if(f < 0.0 || 0.0 < f || f == 0.0) {
+    return false;
+  } else {
+    return true;
+  }
+}
+
+float rand(vec2 co){
+    return fract(sin(dot(co.xy ,vec2(12.9898,78.233))) * 43758.5453);
+}
 
 void main() {
   // Convert fragment coords in pixels to texel coords in [0..1]
@@ -41,95 +64,48 @@ void main() {
   vec2 rank4 = texture2D(crypto02rgb, xy).rg;
   vec2 rank5 = vec2(texture2D(crypto02rgb, xy).b, texture2D(crypto02a, xy).r);
 
-  // Accumulate opacity from the ranks for each picked ID
   vec4 result = vec4(0.0);
-  if(separatergb) {
-    if(enabler) {
-      if(rank0.x == pickr.r) result.r += rank0.y;
-      if(rank1.x == pickr.r) result.r += rank1.y;
-      if(rank2.x == pickr.r) result.r += rank2.y;
-      if(rank3.x == pickr.r) result.r += rank3.y;
-      if(rank4.x == pickr.r) result.r += rank4.y;
-      if(rank5.x == pickr.r) result.r += rank5.y;
-    }
-    if(enableg) {
-      if(rank0.x == pickg.r) result.g += rank0.y;
-      if(rank1.x == pickg.r) result.g += rank1.y;
-      if(rank2.x == pickg.r) result.g += rank2.y;
-      if(rank3.x == pickg.r) result.g += rank3.y;
-      if(rank4.x == pickg.r) result.g += rank4.y;
-      if(rank5.x == pickg.r) result.g += rank5.y;
-    }
-    if(enableb) {
-      if(rank0.x == pickb.r) result.b += rank0.y;
-      if(rank1.x == pickb.r) result.b += rank1.y;
-      if(rank2.x == pickb.r) result.b += rank2.y;
-      if(rank3.x == pickb.r) result.b += rank3.y;
-      if(rank4.x == pickb.r) result.b += rank4.y;
-      if(rank5.x == pickb.r) result.b += rank5.y;
-    }
+
+  // Massage the rank0 ID into a more uniform range
+  float seed, e;
+  seed = frexp(rank0.x, e);
+  if(isnan(seed)) seed = frexp(-rank0.x, e);
+  if(isnan(seed)) seed = 0.0;
+
+  // Seed red/green noise with it
+  result.r = rand(vec2(seed, seed));
+  result.g = rand(vec2(seed, 5.0 * seed));
+
+  // Reduce contrast and add some outlines from the rank0 coverage
+  if(result.rg != vec2(0.0)) result.rg = mix(result.rg, vec2(0.3), 0.5);
+  result = mix(result, result * rank0.y, 0.5);
+
+  // Secrete the rank0 ID in the blue channel, but always negative
+  if(rank0.x < 0.0) {
+    result.rg *= 256.0;
+    result.rg = floor(result.rg);
+    result.rg /= 256.0;
+    result.b = rank0.x;
   } else {
-    if(enableresult) {
-      if(rank0.x == pickresult.r) result.rgb += vec3(rank0.y);
-      if(rank1.x == pickresult.r) result.rgb += vec3(rank1.y);
-      if(rank2.x == pickresult.r) result.rgb += vec3(rank2.y);
-      if(rank3.x == pickresult.r) result.rgb += vec3(rank3.y);
-      if(rank4.x == pickresult.r) result.rgb += vec3(rank4.y);
-      if(rank5.x == pickresult.r) result.rgb += vec3(rank5.y);
-    }
+    result.rg *= 256.0;
+    result.rg = floor(result.rg);
+    result.rg += vec2(0.5);
+    result.rg /= 256.0;
+    result.b = -rank0.x;
   }
 
-  if(enablem) {
-    if(rank0.x == pickm.r) result.a += rank0.y;
-    if(rank1.x == pickm.r) result.a += rank1.y;
-    if(rank2.x == pickm.r) result.a += rank2.y;
-    if(rank3.x == pickm.r) result.a += rank3.y;
-    if(rank4.x == pickm.r) result.a += rank4.y;
-    if(rank5.x == pickm.r) result.a += rank5.y;
+  float pick = pickresult.b;
+  if(fract(pickresult.r * 256.0) > 0.0) {
+    pick = -pick;
   }
+  if(rank0.x == pick) result.a += rank0.y;
+  if(rank1.x == pick) result.a += rank1.y;
+  if(rank2.x == pick) result.a += rank2.y;
+  if(rank3.x == pick) result.a += rank3.y;
+  if(rank4.x == pick) result.a += rank4.y;
+  if(rank5.x == pick) result.a += rank5.y;
 
-  if(combine) {
-    // Combine the picked mattes.  Since our mattes are disjoint, we just add them,
-    // although if for some reason the user has picked the same ID multiple times, this will
-    // result in values over 1.0
-    if(separatergb) {
-      result = vec4(result.r + result.g + result.b + result.a);
-    } else {
-      result = vec4(result.r + result.a);
-    }
-  }
-
-  // If the temporary inspector widget is inside the image, output the first three mattes under
-  // it into RGB
-  if(0.0 < inspect.x && inspect.x < 1.0 && 0.0 < inspect.y && inspect.y < 1.0) {
-    vec2 inspectrank0 = texture2D(crypto00rgb, inspect).rg;
-    vec2 inspectrank1 = vec2(texture2D(crypto00rgb, inspect).b, texture2D(crypto00a, inspect).r);
-    vec2 inspectrank2 = texture2D(crypto01rgb, inspect).rg;
-    vec2 inspectrank3 = vec2(texture2D(crypto01rgb, inspect).b, texture2D(crypto01a, inspect).r);
-    vec2 inspectrank4 = texture2D(crypto02rgb, inspect).rg;
-    vec2 inspectrank5 = vec2(texture2D(crypto02rgb, inspect).b, texture2D(crypto02a, inspect).r);
-    result = vec4(0.0);
-    if(rank0.x == inspectrank0.r) result.ra += rank0.yy;
-    if(rank1.x == inspectrank0.r) result.ra += rank1.yy;
-    if(rank2.x == inspectrank0.r) result.ra += rank2.yy;
-    if(rank3.x == inspectrank0.r) result.ra += rank3.yy;
-    if(rank4.x == inspectrank0.r) result.ra += rank4.yy;
-    if(rank5.x == inspectrank0.r) result.ra += rank5.yy;
-
-    if(rank0.x == inspectrank1.r) result.g += rank0.y;
-    if(rank1.x == inspectrank1.r) result.g += rank1.y;
-    if(rank2.x == inspectrank1.r) result.g += rank2.y;
-    if(rank3.x == inspectrank1.r) result.g += rank3.y;
-    if(rank4.x == inspectrank1.r) result.g += rank4.y;
-    if(rank5.x == inspectrank1.r) result.g += rank5.y;
-
-    if(rank0.x == inspectrank2.r) result.b += rank0.y;
-    if(rank1.x == inspectrank2.r) result.b += rank1.y;
-    if(rank2.x == inspectrank2.r) result.b += rank2.y;
-    if(rank3.x == inspectrank2.r) result.b += rank3.y;
-    if(rank4.x == inspectrank2.r) result.b += rank4.y;
-    if(rank5.x == inspectrank2.r) result.b += rank5.y;
-  }
+  result.rg += result.a;
 
   gl_FragColor = result;
 }
